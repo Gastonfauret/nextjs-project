c694173c73d83559c40c0e57256fa3c09c9e21d4
Gastonfauret <Gastonfauret@gmail.com>
2024-11-12 11:03:42 -0300

    page.tsx on products was modified for third time

--- src/app/productos/[productId]/comentarios/[commentId]/page.tsx
+++ src/app/productos/[productId]/comentarios/[commentId]/page.tsx
@@ -1,21 +1,11 @@
-// src/app/productos/[productId]/comentarios/[commentId]/page.tsx
-
-export function generateMetadata({ params }: { params: { productId: string; commentId: string } }) {
-  return {
-    title: `Comentario ${params.commentId} del Producto ${params.productId}`,
-  };
-}
-
-export default function CommentDetail({
-  params,
-}: {
+type PageProps = {
   params: { productId: string; commentId: string };
-}) {
+};
+
+export default function CommentDetail({ params }: PageProps) {
   return (
-    <div className="flex items-center justify-center h-full text-xl bg-red-100">
-      <h1>
-        Comentario {params.commentId} del Producto {params.productId}
-      </h1>
+    <div>
+      <h1>Comentario {params.commentId} del Producto {params.productId}</h1>
     </div>
   );
 }
