7c51df58565d8f917f3bd15982543af27d294c6f
Gastonfauret <Gastonfauret@gmail.com>
2024-11-12 10:21:32 -0300

    Changes on .eslintrc files

--- src/app/API/route.ts
+++ src/app/API/route.ts
@@ -1,3 +1,4 @@
-export async function GET (): Promise<Response> {
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export async function GET (request: Request): Promise<Response> {
     return new Response("Hola Mundo, esta es la API Rest")
 }
