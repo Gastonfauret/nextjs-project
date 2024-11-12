94ff201aa794067f2ffd8c06dbf9aa080b38cb4a
Gastonfauret <Gastonfauret@gmail.com>
2024-11-12 10:23:14 -0300

    request type deleted

--- src/app/API/route.ts
+++ src/app/API/route.ts
@@ -1,4 +1,4 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-export async function GET (request: Request): Promise<Response> {
+export async function GET (): Promise<Response> {
     return new Response("Hola Mundo, esta es la API Rest")
 }
