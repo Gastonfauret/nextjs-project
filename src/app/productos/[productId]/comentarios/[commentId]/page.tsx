eba9802fc1f59410e6c8de29dadea6b15456967f
Gastonfauret <Gastonfauret@gmail.com>
2024-11-12 10:55:28 -0300

    page.tsx on products was modified

--- src/app/productos/[productId]/comentarios/[commentId]/page.tsx
+++ src/app/productos/[productId]/comentarios/[commentId]/page.tsx
@@ -1,8 +1,10 @@
-export default function CommentDetail({
-  params,
-}: {
+import { GetServerSideProps } from 'next';
+
+type Props = {
   params: { productId: string; commentId: string };
-}) {
+};
+
+export default function CommentDetail({ params }: Props) {
   return (
     <div className="flex items-center justify-center h-full text-xl bg-red-100">
       <h1>
@@ -11,3 +13,16 @@ export default function CommentDetail({
     </div>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { productId, commentId } = context.params!;
+
+  return {
+    props: {
+      params: {
+        productId,
+        commentId,
+      },
+    },
+  };
+};
